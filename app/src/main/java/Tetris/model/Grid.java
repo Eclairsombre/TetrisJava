36c27b3f3ed77cb0db0b42f46d4c94436f9dbb65
Eclairsombre <alexandre.thny@gmail.com>
2025-05-13 12:54:35 +0200

    feat : init grid

--- app/src/main/java/Tetris/model/Grid.java
+++ app/src/main/java/Tetris/model/Grid.java
@@ -1,5 +1,36 @@
 package Tetris.model;
 
-public class Grid {
+import java.util.Observable;
+
+public class Grid extends Observable {
+    private final int width;
+    private final int height;
+    private final String[][] grid;
+
+    public Grid(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.grid = new String[width][height];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public String[][] getGrid() {
+        return grid;
+    }
+
+    public void setCell(int x, int y, String value) {
+        if (x >= 0 && x < width && y >= 0 && y < height) {
+            grid[x][y] = value;
+            setChanged();
+            notifyObservers(grid[x][y]);
+        }
+    }
 
 }
